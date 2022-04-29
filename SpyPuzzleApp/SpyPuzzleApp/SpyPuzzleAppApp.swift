04f2a244576cd3de500292e67f625b316ae6e1d4
jackpal <jack.palevich@gmail.com>
2022-04-28 20:44:48 -0700

    Create app from Xcode iOS App template.

---
+++ SpyPuzzleApp/SpyPuzzleApp/SpyPuzzleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpyPuzzleAppApp.swift
+//  SpyPuzzleApp
+//
+//  Created by Jack Palevich on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SpyPuzzleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
